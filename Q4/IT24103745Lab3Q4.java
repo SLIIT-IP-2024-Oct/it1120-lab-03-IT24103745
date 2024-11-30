ad2ea5fed899c0b89d499c656397e9697cbdb7f7
IT24103745 <it24103745@my.sliit.lk>
2024-11-30 14:01:30 +0530

    Add IT24103745Lab3Q4.java to Q4

---
+++ Q4/IT24103745Lab3Q4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class IT24103745Lab3Q4 {
+    public static void main(String[] args) {
+        int number , digit1 , digit2 , digit3 , digit4 , digit5;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a five-digit number: ");
+        number = input.nextInt();
+
+	digit1 = number/10000;
+	number %=10000;
+
+	digit2 = number/1000;
+	number %=1000;
+
+	digit3 = number/100;
+	number %=100;
+
+	digit4 = number/10;
+	number %=10;
+
+	digit5 = number;
+	System.out.println(digit1 + " " + digit2 + " " + digit3 + " " + digit4 + " " + digit5);
+}
+}
